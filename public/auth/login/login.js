b24202decececa4d3f9324235eb19de1fa89898a
nnarct <nannapat1551@gmail.com>
2021-12-09 22:33:39 +0700

    toggle profile menu

---
+++ public/auth/login/login.js
@@ -0,0 +1,142 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyADVCDx2u4eyXan_CTvJ2N-zRJ6zT5eBPw",
+    authDomain: "kodangnungsue.firebaseapp.com",
+    databaseURL: "https://kodangnungsue-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "kodangnungsue",
+    storageBucket: "kodangnungsue.appspot.com",
+    messagingSenderId: "210511003247",
+    appId: "1:210511003247:web:597d153c1b27be3536979a",
+    measurementId: "G-QY54X5370W"
+};
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth()
+const database = firebase.firestore()
+
+let buttonSend = document.getElementById('button-send')
+buttonSend.addEventListener('click', login);
+let passwordFeild = document.getElementById('floatingPassword')
+passwordFeild.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+        login();
+    }
+})
+
+
+function login() {
+    let email = document.getElementById('floatingInput').value
+    let password = document.getElementById('floatingPassword').value
+
+    if (validate_field(email) == false || validate_field(password) == false) {
+        Swal.fire(
+            'Error',
+            'Please fill out all text fields',
+            'error'
+        )
+        return
+    }
+    if (validate_email(email) == false) {
+        Swal.fire(
+            'Error',
+            'Invalid email format',
+            'error'
+        )
+        return
+    }
+    if (validate_password(password) == false) {
+        Swal.fire(
+            'Error',
+            'Enter a password of 8-16 characters',
+            'error'
+        )
+        return
+    }
+    Swal.fire({
+        title: 'Please wait',
+        text: 'System is processing',
+        icon: 'info',
+        allowEscapeKey: false,
+        allowOutsideClick: false,
+        didOpen: () => {
+            Swal.showLoading()
+        }
+    });
+
+    auth.signInWithEmailAndPassword(email, password)
+        .then(function () {
+            var user = auth.currentUser
+            var database_ref = database.collection('users').doc(user.uid)
+
+            var user_data = {
+                last_login: Date.now()
+            }
+            database_ref.update(user_data)
+
+            document.getElementById('floatingInput').value = '';
+            document.getElementById('floatingPassword').value = '';
+
+            database_ref.get().then((doc) => {
+                let userDetail = {
+                    id: doc.id,
+                    address: doc.data().address,
+                    email: doc.data().email,
+                    Name: doc.data().name,
+                    surname: doc.data().surname,
+                    phone: doc.data().phone,
+                    type: doc.data().type,
+                    last_login: doc.data().last_login
+                }
+                Swal.fire(
+                    'Login Success',
+                    'Welcome to homepage',
+                    'success'
+                )               
+                localStorage.setItem('userNow', JSON.stringify(userDetail));
+                sessionStorage.setItem('userDetail', JSON.stringify(userDetail));
+                setTimeout(() => {
+                    location.href = "../index.html";
+                }, 1500)
+            })
+
+
+        })
+        .catch(function (error) {
+            var error_code = error.code
+            var error_message = error.message
+
+            Swal.fire(
+                'Error',
+                error_message,
+                'error'
+            )
+        })
+}
+
+function validate_email(email) {
+    expression = /^[^@]+@\w+(\.\w+)+\w$/
+    if (expression.test(email) == true) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+function validate_password(password) {
+    if (password.toString < 8 || password.toString > 16) {
+        return false
+    } else {
+        return true
+    }
+}
+
+function validate_field(field) {
+    if (field == null) {
+        return false
+    }
+
+    if (field.length <= 0) {
+        return false
+    } else {
+        return true
+    }
+}
---
+++ public/index.js
@@ -0,0 +1,431 @@
+// <----------------------------------- เรียกใช้ Database --------------------------------------->
+var firebaseConfig = {
+    apiKey: "AIzaSyADVCDx2u4eyXan_CTvJ2N-zRJ6zT5eBPw",
+    authDomain: "kodangnungsue.firebaseapp.com",
+    databaseURL: "https://kodangnungsue-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "kodangnungsue",
+    storageBucket: "kodangnungsue.appspot.com",
+    messagingSenderId: "210511003247",
+    appId: "1:210511003247:web:597d153c1b27be3536979a",
+    measurementId: "G-QY54X5370W"
+};
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth()
+const database = firebase.firestore()
+const storageRef = firebase.storage().ref()
+
+// <----------------------------------- ส่วนดำเนินการ --------------------------------------->
+
+let userDetail = JSON.parse(localStorage.getItem('userNow'));
+let userLogin = false;
+let headerButton = document.querySelector('.button');
+let headerBasket = document.querySelector('.rmenu');
+headerBasket.style.display = 'none';
+
+auth.onAuthStateChanged((user) => {
+    if (user) {
+        userLogin = true;
+        headerButton.style.display = 'none';
+        headerBasket.style.removeProperty('display');
+        reset();
+        showProductSearch('');
+    }
+    else {
+        userLogin = false;
+        headerBasket.style.display = 'none';
+        headerButton.style.removeProperty('display');
+        reset();
+        showProductSearch('');
+
+    }
+});
+
+let headingTitle = document.querySelector('#heading-title');
+let inputText = document.querySelector('.input > input');
+inputText.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+        if (inputText.value === '') {
+            headingTitle.innerHTML = 'หนังสือแนะนำ';
+        }
+        else {
+            headingTitle.innerHTML = 'รายการที่ค้นหา';
+        }
+        reset();
+        showProductSearch(inputText.value);
+    }
+})
+
+let cates = document.querySelectorAll('#category > h5');
+let subCates = document.querySelectorAll('#category > li');
+
+cates.forEach((cate) => {
+    cate.addEventListener('click', () => {
+        headingTitle.innerHTML = cate.innerText;
+        reset();
+        showProductType(cate.innerText);
+    });
+});
+
+subCates.forEach((subCate, index) => {
+    subCate.addEventListener('click', () => {
+        if (index >= 0 && index <= 2) {
+            headingTitle.innerHTML = cates[0].innerText + ': ' + subCate.innerText;
+            reset();
+            showProductType(subCate.innerText);
+        }
+        else if (index >= 3 && index <= 5) {
+            headingTitle.innerHTML = cates[1].innerText + ': ' + subCate.innerText;
+            reset();
+            showProductType(subCate.innerText);
+        }
+    });
+});
+// <------------------------------ส่วนฟังก์ชันต่างๆ--------------------------------------->
+
+function reset() {
+    let allProduct = document.querySelector('.product');
+    allProduct.innerHTML = null;
+    allProduct.append(document.createElement('br'));
+}
+
+function showProductSearch(searchText) {
+    let database_ref = database.collection('products');
+
+    // All Product Box
+    let allProduct = document.querySelector('.product');
+
+    database_ref.get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            let product = {
+                id: doc.id,
+                amount: doc.data().amount,
+                condition: doc.data().condition,
+                date: doc.data().date,
+                detail: doc.data().detail,
+                imgLength: doc.data().imgLength,
+                price: doc.data().price,
+                name: doc.data().productName,
+                province: doc.data().province,
+                rating: doc.data().rating,
+                seller: doc.data().seller,
+                type: doc.data().type,
+                userId: doc.data().userId
+            };
+
+            if (product.name.includes(searchText)) {
+                // กรอบสินค้า
+                let pbox = document.createElement('div');
+                pbox.classList.add('pbox');
+
+                // กรอบรูป
+                let pimg = document.createElement('div');
+                pimg.classList.add('pimg');
+
+                // ตัวเชื่อมไปหน้าสินค้า 1
+                let connect1 = document.createElement('a');
+                connect1.href = 'prod.html';
+                connect1.addEventListener('click', function () {
+                    sessionStorage.setItem('productSelected', JSON.stringify(product));
+                });
+
+                // ภาพสินค้า
+                let image = document.createElement('img');
+                image.src = 'src/noImage.png';
+                storageRef.child(product.userId + '/' + product.id + '/image1').getDownloadURL()
+                    .then((url) => {
+                        image.src = url;
+                    }).catch((error) => {
+                        Swal.fire(
+                            'Error',
+                            error.message + '\n Please refresh webpage',
+                            'error'
+                        )
+                    })
+
+                // ดูเพิ่มเติม
+                let moreBtn = document.createElement('div');
+                moreBtn.classList.add('overlay-up');
+                let moreText = document.createElement('span')
+                moreText.classList.add('text-up');
+                moreText.innerHTML = 'ดูเพิ่มเติม';
+                moreBtn.append(moreText);
+
+                // รวมส่วนภาพ
+                connect1.append(image, moreBtn);
+                pimg.append(connect1);
+
+                // ส่วนเนื้อหา
+                let pde = document.createElement('div');
+                pde.classList.add('pde');
+
+                // ตัวเชื่อมไปหน้าสินค้า 2
+                let connect2 = document.createElement('a');
+                connect2.href = 'prod.html';
+                connect2.addEventListener('click', function () {
+                    sessionStorage.setItem('productSelected', JSON.stringify(product));
+                });
+
+                // ชื่อสินค้า
+                let productName = document.createElement('span')
+                productName.classList.add('pname');
+                productName.innerHTML = product.name;
+
+                // ประกอบตัวเชื่อมกับชื่อสินค้า
+                connect2.append(productName);
+
+                // ตัวเชื่อมไปหน้าสินค้า 3
+                let connect3 = document.createElement('a');
+                connect3.href = 'prod.html';
+                connect3.addEventListener('click', function () {
+                    sessionStorage.setItem('productSelected', product);
+                });
+
+                // ราคาสินค้า
+                let price = document.createElement('span')
+                price.classList.add('pprice');
+                price.innerHTML = '฿' + product.price;
+
+                // ประกอบตัวเชื่อมกับราคา
+                connect3.append(price);
+
+                // คะแนนสินค้า
+                let rating = document.createElement('div');
+                rating.classList.add('star');
+                let productRate = product.rating;
+                for (let i = 0; i < 5; i++) {
+                    let star = document.createElement('span');
+                    star.classList.add('fa', 'fa-star');
+                    if (productRate >= 1) {
+                        star.classList.add('checked');
+                        productRate--;
+                    }
+                    else if (productRate !== 0) {
+                        star.classList.replace('fa-star', 'fa-star-half-full');
+                        star.classList.add('checked')
+                        productRate = 0;
+                    }
+                    rating.append(star);
+                }
+
+                // ปุ่มใส่ตะกร้า
+                let cart = document.createElement('span');
+                cart.classList.add('cart');
+
+                // ไอคอนตะกร้า
+                let cartImg = document.createElement('img');
+                cartImg.src = "src/icon/addcart.png";
+                cartImg.id = 'basket';
+
+                // ประกอบปุ่มตะกร้า
+                cart.append(cartImg);
+                cart.innerHTML += 'หยิบใส่ตะกร้า';
+
+                if (userLogin) {
+                    cart.classList.add('n');
+                }
+                else {
+                    // สำหรับยังไม่ล็อคอิน
+                    let noLogin = document.createElement('div');
+                    noLogin.classList.add('overlay');
+                    let connectLogin = document.createElement('a');
+                    connectLogin.classList.add('text');
+                    connectLogin.href = 'login/login.html';
+                    connectLogin.innerHTML = 'Please Login before...';
+                    noLogin.append(connectLogin);
+                    cart.append(noLogin);
+                }
+
+                // ประกอบเข้าทั้งหมด
+                pde.append(connect2, connect3, rating, cart);
+                pbox.append(pimg, pde);
+                allProduct.append(pbox);
+            }
+
+        });
+
+    });
+
+}
+
+function showProductType(type) {
+    let database_ref = database.collection('products');
+
+    // All Product Box
+    let allProduct = document.querySelector('.product');
+
+    database_ref.get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            let product = {
+                id: doc.id,
+                amount: doc.data().amount,
+                condition: doc.data().condition,
+                date: doc.data().date,
+                detail: doc.data().detail,
+                imgLength: doc.data().imgLength,
+                price: doc.data().price,
+                name: doc.data().productName,
+                province: doc.data().province,
+                rating: doc.data().rating,
+                seller: doc.data().seller,
+                type: doc.data().type,
+                userId: doc.data().userId
+            };
+
+            if (product.type.includes(type)) {
+                // กรอบสินค้า
+                let pbox = document.createElement('div');
+                pbox.classList.add('pbox');
+
+                // กรอบรูป
+                let pimg = document.createElement('div');
+                pimg.classList.add('pimg');
+
+                // ตัวเชื่อมไปหน้าสินค้า 1
+                let connect1 = document.createElement('a');
+                connect1.href = 'prodn.html';
+                connect1.addEventListener('click', function () {
+                    sessionStorage.setItem('productSelected', JSON.stringify(product));
+                });
+
+                // ภาพสินค้า
+                let image = document.createElement('img');
+                image.src = 'product/noImage.png';
+                storageRef.child(product.userId + '/' + product.id + '/image1').getDownloadURL()
+                    .then((url) => {
+                        image.src = url;
+                    }).catch((error) => {
+                        Swal.fire(
+                            'Error',
+                            error.message + '\n Please refresh webpage',
+                            'error'
+                        )
+                    })
+
+                // ดูเพิ่มเติม
+                let moreBtn = document.createElement('div');
+                moreBtn.classList.add('overlay-up');
+                let moreText = document.createElement('span')
+                moreText.classList.add('text-up');
+                moreText.innerHTML = 'ดูเพิ่มเติม';
+                moreBtn.append(moreText);
+
+                // รวมส่วนภาพ
+                connect1.append(image, moreBtn);
+                pimg.append(connect1);
+
+                // ส่วนเนื้อหา
+                let pde = document.createElement('div');
+                pde.classList.add('pde');
+
+                // ตัวเชื่อมไปหน้าสินค้า 2
+                let connect2 = document.createElement('a');
+                connect2.href = 'prodn.html';
+                connect2.addEventListener('click', function () {
+                    sessionStorage.setItem('productSelected', JSON.stringify(product));
+                });
+
+                // ชื่อสินค้า
+                let productName = document.createElement('span')
+                productName.classList.add('pname');
+                productName.innerHTML = product.name;
+
+                // ประกอบตัวเชื่อมกับชื่อสินค้า
+                connect2.append(productName);
+
+                // ตัวเชื่อมไปหน้าสินค้า 3
+                let connect3 = document.createElement('a');
+                connect3.href = 'prodn.html';
+                connect3.addEventListener('click', function () {
+                    sessionStorage.setItem('productSelected', product);
+                });
+
+                // ราคาสินค้า
+                let price = document.createElement('span')
+                price.classList.add('pprice');
+                price.innerHTML = '฿' + product.price;
+
+                // ประกอบตัวเชื่อมกับราคา
+                connect3.append(price);
+
+                // คะแนนสินค้า
+                let rating = document.createElement('div');
+                rating.classList.add('star');
+                let productRate = product.rating;
+                for (let i = 0; i < 5; i++) {
+                    let star = document.createElement('span');
+                    star.classList.add('fa', 'fa-star');
+                    if (productRate >= 1) {
+                        star.classList.add('checked');
+                        productRate--;
+                    }
+                    else if (productRate !== 0) {
+                        star.classList.replace('fa-star', 'fa-star-half-full');
+                        star.classList.add('checked')
+                        productRate = 0;
+                    }
+                    rating.append(star);
+                }
+
+                // ปุ่มใส่ตะกร้า
+                let cart = document.createElement('span');
+                cart.classList.add('cart');
+
+                // ไอคอนตะกร้า
+                let cartImg = document.createElement('img');
+                // cartImg.src = "src/icon/addcart.png";
+                cartImg.src = 'product/addcart.png';
+                cartImg.id = 'basket';
+
+                // ประกอบปุ่มตะกร้า
+                cart.append(cartImg);
+                cart.innerHTML += 'หยิบใส่ตะกร้า';
+
+                if (userLogin) {
+                    cart.classList.add('n');
+                }
+                else {
+                    // สำหรับยังไม่ล็อคอิน
+                    let noLogin = document.createElement('div');
+                    noLogin.classList.add('overlay');
+                    let connectLogin = document.createElement('a');
+                    connectLogin.classList.add('text');
+                    connectLogin.href = 'login/login.html';
+                    connectLogin.innerHTML = 'Please Login before...';
+                    noLogin.append(connectLogin);
+                    cart.append(noLogin);
+                }
+
+                // ประกอบเข้าทั้งหมด
+                pde.append(connect2, connect3, rating, cart);
+                pbox.append(pimg, pde);
+                allProduct.append(pbox);
+            }
+
+        });
+
+    });
+
+}
+
+function showOnlyProduct() {
+    loadProductOnlyUser(userDetail.id).then((result) => {
+        products = result;
+        products.forEach((product) => {
+            storageRef.child(product.userId + '/' + product.id).listAll()
+                .then((res) => {
+                    res.items.forEach((itemRef) => {
+                        console.log(itemRef.name)
+                        itemRef.getDownloadURL().then((url) => {
+                            console.log(url);
+                        });
+                    });
+                }).catch((error) => {
+                    Swal.fire(
+                        'Error',
+                        error.message,
+                        'error'
+                    )
+                })
+        });
+    });
+}
