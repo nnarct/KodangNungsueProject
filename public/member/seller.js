5fb485d647722dd8df921cd27d2c3f5b28ebeff9
nnarct <nannapat1551@gmail.com>
2021-12-11 15:04:13 +0700

    Co-authored-by: AKKARACHAI PAWONGJIT <akkarachai.6787@mail.kmutt.ac.th>

--- public/member/seller.js
+++ public/member/seller.js
@@ -1,4 +1,43 @@
-document.getElementById("seller").onclick = function () {
-    document.getElementById("toEdit-btn").style.display = "block";
-    document.
+document.getElementById("sellerInfo").onclick = function () {
+    document.getElementById("sellerInfo").style.background = "rgb(155, 231, 97)" ;
+    document.getElementById("sellerStorage").style.background = "white";
+    document.getElementById("ship").style.background = "white";
+    document.getElementById("addProduct").style.background = "white";
+    document.getElementById("S-Info").style.display = "block";
+    document.getElementById("S-Storage").style.display = "none";
+    document.getElementById("S-Ship").style.display = "none";
+    document.getElementById("S-AddProduct").style.display = "none";
+}
+document.getElementById("sellerStorage").onclick = function () {
+    document.getElementById("sellerInfo").style.background = "white" ;
+    document.getElementById("sellerStorage").style.background = "rgb(155, 231, 97)";
+    document.getElementById("ship").style.background = "white";
+    document.getElementById("addProduct").style.background = "white";
+
+    document.getElementById("S-Info").style.display = "none";
+    document.getElementById("S-Storage").style.display = "block";
+    document.getElementById("S-Ship").style.display = "none";
+    document.getElementById("S-AddProduct").style.display = "none";
+}
+document.getElementById("ship").onclick = function () {
+    document.getElementById("sellerInfo").style.background = "white";
+    document.getElementById("sellerStorage").style.background = "white";
+    document.getElementById("ship").style.background = "rgb(155, 231, 97)";
+    document.getElementById("addProduct").style.background = "white";
+
+    document.getElementById("S-Info").style.display = "none";
+    document.getElementById("S-Storage").style.display = "none";
+    document.getElementById("S-Ship").style.display = "block";
+    document.getElementById("S-AddProduct").style.display = "none";
+}
+document.getElementById("addProduct").onclick = function () {
+   document.getElementById("sellerInfo").style.background = "white" ;
+    document.getElementById("sellerStorage").style.background = "white";
+    document.getElementById("ship").style.background = "white";
+    document.getElementById("addProduct").style.background = "rgb(155, 231, 97)";
+    
+    document.getElementById("S-Info").style.display = "none";
+    document.getElementById("S-Storage").style.display = "none";
+    document.getElementById("S-Ship").style.display = "none";
+    document.getElementById("S-AddProduct").style.display = "block";
 }
